1b824b4c0df70c25467d458e6eb9eeb9d0d9d155
Ali Skinner <awskinner26@gmail.com>
2017-05-10 09:03:53 +0100

    added ToDoApp component

---
+++ app/components/ToDoApp.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+
+const ToDoApp = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        <h1>ToDoApp.jsx</h1>
+      </div>
+    );
+  }
+
+});
+
+module.exports = ToDoApp;
